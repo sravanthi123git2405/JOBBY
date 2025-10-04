5ce26e1f0db842d4ea3e94569bd979795bc8077d
sravanthi123git2405 <sravanthipothuganti4@gmail.com>
2025-10-04 08:50:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './Component/Home';
+import LoginPage from './Component/LoginPage';
+import SignupPage from './Component/SignupPage';
+import Jobs from './Component/Jobs';
+import JobItemDetails from './Component/JobItemDetails';
+import ProtectedRoute from './Component/ProtectedRoute'; 
+import AppliedJobs from './Component/AppliedJobs';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/signup" element={<SignupPage />} />
+    <Route exact path="/applied-jobs" element={<AppliedJobs />} />
+
+
+    <Route
+      path="/home"
+      element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/jobs"
+      element={
+        <ProtectedRoute>
+          <Jobs />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/jobs/:id"
+      element={
+        <ProtectedRoute>
+          <JobItemDetails />
+        </ProtectedRoute>
+      }
+    />
+
+    <Route path="*" element={<Navigate to="/login" replace />} />
+  </Routes>
+);
 
 export default App;
---
+++ src/Component/withRouter.js
@@ -0,0 +1,11 @@
+// withRouter.js
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
+
+export function withRouter(Component) {
+  return props => {
+    const params = useParams()
+    const location = useLocation()
+    const navigate = useNavigate()
+    return <Component {...props} params={params} location={location} navigate={navigate} />
+  }
+}
